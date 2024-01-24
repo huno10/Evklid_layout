7e84afdeb7c34c5e3bf276ded36445ed31201f95
Unan <hunanhopar@mail.ru>
2024-01-24 15:41:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,152 @@
+let burger = document.querySelector('.header__burger-menu');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.header__link');
+
+burger.addEventListener("click",
+  function () {
+
+    burger.classList.toggle('header__burger-menu--active');
+
+    menu.classList.toggle('header__nav--active');
+
+    document.body.classList.toggle('stop-scroll');
+
+  })
+
+menuLinks.forEach(function (el) {
+  el.addEventListener("click", function () {
+    burger.classList.remove("header__burger-menu--active");
+
+    menu.classList.remove('header__nav--active');
+
+    document.body.classList.remove('stop-scroll');
+  })
+})
+
+let tabsBtn = document.querySelectorAll('.work__item-btn');
+let tabsItem = document.querySelectorAll('.work__info-item');
+
+tabsBtn.forEach(function (element) {
+  element.addEventListener('click', function (e) {
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function (btn) { btn.classList.remove('work__item-btn--active') });
+    e.currentTarget.classList.add('work__item-btn--active');
+
+    tabsItem.forEach(function (element) { element.classList.remove('work__info-item--active') });
+    document.querySelector(`[data-target="${path}"]`).classList.add('work__info-item--active');
+
+  });
+});
+
+var swiper = new Swiper(".mySwiper", {
+
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+});
+
+
+(() => {
+  new Accordion(".js-accordion-container");
+})();
+
+
+// search
+
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+
+  search.addEventListener("animationend", function (evt) {
+    if (this._isOpened) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+      this._isOpened = false;
+    } else {
+      this._isOpened = true;
+    }
+  });
+
+  search.addEventListener('click', function(evt) {
+    evt._isSearch = true;
+  });
+
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
+
+    if (
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
+    ) {
+      search.classList.add(params.activeClass);
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+
+  document.body.addEventListener('click', function (evt) {
+    if (!evt._isSearch && search._isOpened) {
+      openBtn.disabled = false;
+      search.classList.add(params.hiddenClass);
+    }
+  });
+}
+
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+
+  search.addEventListener("animationend", function (evt) {
+    if (this._isOpened) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+      this._isOpened = false;
+    } else {
+      this._isOpened = true;
+    }
+  });
+
+  search.addEventListener('click', function(evt) {
+    evt._isSearch = true;
+  });
+
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
+
+    if (
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
+    ) {
+      search.classList.add(params.activeClass);
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+
+  document.body.addEventListener('click', function (evt) {
+    if (!evt._isSearch && search._isOpened) {
+      openBtn.disabled = false;
+      search.classList.add(params.hiddenClass);
+    }
+  });
+}
+
+setSearch({
+  openBtnClass: "js-open-search", // класс кнопки открытия
+  closeBtnClass: "js-close-search", // класс кнопки закрытия
+  searchClass: "js-form", // класс формы поиска
+  activeClass: "is-opened", // класс открытого состояния
+  hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+});
